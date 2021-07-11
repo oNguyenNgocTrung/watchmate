2c8585a473f0967016e136e17c1e4a3aed8e5c47
Trung Nguyen <trungnn142@gmail.com>
2021-07-11 18:18:58 +0700

    Django relationship and nested serializer

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class WatchList(models.Model):
-  title = models.CharField(max_length=50)
-  storyline = models.CharField(max_length=200)
-  active = models.BooleanField(default=True)
-  created = models.DateTimeField(auto_now_add=True)
-
-  def __str__(self):
-    return self.title
-
 class StreamPlatform(models.Model):
   name = models.CharField(max_length=30)
   about = models.CharField(max_length=150)
@@ -17,3 +8,14 @@ class StreamPlatform(models.Model):
 
   def __str__(self):
     return self.name
+
+class WatchList(models.Model):
+  title = models.CharField(max_length=50)
+  storyline = models.CharField(max_length=200)
+  active = models.BooleanField(default=True)
+  platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
+
